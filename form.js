29e9197ee695759a588ce6475f29ae7b93ddee3b
ilman hidayat <44702465+ilmanidayat@users.noreply.github.com>
2022-03-08 15:05:34 +0800

    Add files via upload

---
+++ form.js
@@ -0,0 +1,40 @@
+function buat_login() {
+  var elem = document.getElementById('x');
+  elem.parentElement.removeChild(elem);
+
+  var element = document.getElementsByTagName('div')[0]; //div
+
+  var p = document.createElement('p');
+  p.className = 'tulisan_login';
+  p.innerHTML = 'silahkan login';
+  element.appendChild(p);
+
+  var form = document.createElement('form');
+  element.appendChild(form);
+  var label = document.createElement('label');
+  label.innerHTML = 'Username';
+  form.appendChild(label);
+
+  var input_username = document.createElement('input');
+  input_username.type = 'text';
+  input_username.name = 'username';
+  input_username.value = 'ilman';
+  input_username.className = 'form_login';
+  form.appendChild(input_username);
+
+  var label2 = document.createElement('label');
+  label2.innerHTML = 'Password';
+  form.appendChild(label2);
+
+  var input_password = document.createElement('input');
+  input_password.type = 'text';
+  input_password.name = 'password';
+  input_password.className = 'form_login';
+  input_password.value = 'xxx';
+  form.appendChild(input_password);
+
+  var tombol = document.createElement('input');
+  tombol.type = 'submit';
+  tombol.className = 'tombol_login';
+  form.appendChild(tombol);
+}
